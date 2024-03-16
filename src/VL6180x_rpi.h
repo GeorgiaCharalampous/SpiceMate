896cfc074ca802225bf2aaafd692e488399f149e
ViktoriyaKaleva <viktoria.kaleva17@gmail.com>
2024-03-16 15:05:22 +0000

    class definition

--- src/VL6180x_rpi.h
+++ src/VL6180x_rpi.h
@@ -10,3 +10,13 @@
 #endif
 
 static const char could_not_open_i2c[] = "Could not open I2C.\n";
+
+// default i2c address 
+#define DEFAULT_VL6180X_ADDRESS 0x29
+
+// default GPIO pin for the interrupt
+#define DEAFULAT_INT_TO_GPIO 17
+
+class VL6180x_rpi {
+
+};
