207175dc0b959b0b61f6250209bc2f72d12f1e85
ViktoriyaKaleva <viktoria.kaleva17@gmail.com>
2024-03-16 15:22:46 +0000

    added methods in the header

--- src/VL6180x_rpi.h
+++ src/VL6180x_rpi.h
@@ -17,6 +17,32 @@ static const char could_not_open_i2c[] = "Could not open I2C.\n";
 // default GPIO pin for the interrupt
 #define DEAFULAT_INT_TO_GPIO 17
 
+struct VL6180Xsettings{
+    int i2c_bus = 1;
+
+    uint8_t address = DEFAULT_VL6180X_ADDRESS;
+
+    bool initPIGPIO = true;
+};
+
 class VL6180x_rpi {
+    public:
+    /**
+     * destructor 
+    **/
+    ~VL6180x_rpi() {stop();}
+
+    /**
+     * start data acquisition 
+    **/
+   void start (VL6180Xsettings vl6180xsettings = VL6180Xsettings());
+
+    /**
+     * stop data acquisition 
+    **/
+    void stop();
+
+    private:
+    VL6180Xsettings sensorSettings;
 
 };
