db0a1dccd8bd5e5f4231eacd16a236de3f2e8aa9
GeorgiaCharalampous <101838741+GeorgiaCharalampous@users.noreply.github.com>
2024-04-02 01:07:04 +0100

    Initalisation settings semi-defined

--- src/Vibro4_rpi.h
+++ src/Vibro4_rpi.h
@@ -11,13 +11,85 @@
 #define DEBUG
 #endif
 
+static const char could_not_open_i2c[] = "Could not open I2C.\n";
+
 // default i2c address
-#define DEFAULT_VIBRO_ADDRESS   
+#define DEFAULT_VIBRO_ADDRESS
+
+// default GPIO pin for EN pin
+#define DEFAULT_EN_PIN 25
 
 struct VIBRO4_settings{
+    int default_i2c_bus = 1;
+    uint8_t address = DEFAULT_VIBRO_ADDRESS;
+    bool initPIGPIO = true;
+    int int_gpio = DEFAULT_EN_PIN;
+
+    // init setup
+    uint8_t en_high = 1;
+
+    uint8_t init_autocal_mode = MODE_AUTO_CALIBRATION; // idle with internal trigger
+
+    // Auto-calibration register config section
+    uint8_t init_feedBack_reg = (LRA_MODE|FB_BRAKE_FACTOR_3x|LOOP_GAIN_HIGH);
+    uint8_t init_ratedVoltage_reg = RATED_VOLTAGE_DEFAULT; //default needs to change?
+    uint8_t init_odClamp_reg = OD_CLAMP; //default needs to change?
+    uint8_t init_control4_reg = AUTO_CAL_TIME_500um;
+    uint8_t init_control1_reg = DRIVE_TIME; //default needs to change?
+    uint8_t init_control2_reg = SAMPLE_TIME_300um|BLANKING_TIME|IDISS_TIME;
+    uint8_t init_go = VIBRO_GO;
+
+    uint8_t vibro_library = LIBRARY_SELECT_LRA;
     
+    //put on standby after autocal
+    uint8_t standby = STANDBY;
+    uint8_t device_rdy = DEVICE_RDY;
 };
+
+67.66
 
 class VIBRO_rpi{
 
+    public:
+    /**
+     * destructor 
+    **/
+    ~VIBRO_rpi() {stop();}
+
+    /**
+     * auto-callibration  
+    **/
+    void autoCallibration(VIBRO4_settings vibro4settings = VIBRO4_settings());
+
+    /**
+     * Registers the callback on sample arrival.
+     * \param cb Pointer to the callback interface. 
+    **/
+    void registerCallback(VIBRO4callback* cb);
+    
+    /**
+     * Unregisters the callback
+    **/
+    void unRegisterCallback();
+
+    private:
+    VIBRO4_settings motorSettings;
+    VIBRO4callback* motorCallback = nullptr;
+    std::thread proxThread;//Proximity sensor thread
+    int running = 0;
+    void i2c_writeTwoBytes(uint8_t reg, unsigned data);
+    void i2c_writeByte(uint8_t reg, unsigned data);
+    unsigned i2c_readTwoBytes(uint8_t reg);
+    unsigned i2c_readByte(uint8_t reg);
+    int i2c_readConversion();
+
+    const uint8_t reg_congig = 1;
+    const uint8_t reg_lo_thres = 2;
+    const uint8_t reg_hi_thres = 3;
+
+    void dataReady();
+    void run();
+    static void gpioISR(int,int,uint32_t,void* userdata){
+        ((VL6180x_rpi*)userdata)->dataReady();
+    }
 };
--- src/Vibro_reg.h
+++ src/Vibro_reg.h
@@ -21,14 +21,21 @@ enum {
 };
 
 #define VIBRO_RTP_REG           0x02
-#define VIBRO_REGFIELD_REG      0x03        // 6:LRA Library
+#define VIBRO_REGFIELD_REG      0x03        
 enum {
     HIGH_IMP_STATE_DIS          = 0b00000000,
     HIGH_IMP_STATE_ENABLE       = 0b00001000,
     LIBRARY_SELECT_LRA          = 0b00000110, //lib 6 for LRA
 };
 
-#define VIBRO_WAVESEQ_REG       0x04:0x0B
+#define VIBRO_WAV_SEQ_REG1      0x04
+#define VIBRO_WAV_SEQ_REG2      0x05
+#define VIBRO_WAV_SEQ_REG3      0x06
+#define VIBRO_WAV_SEQ_REG4      0x07
+#define VIBRO_WAV_SEQ_REG5      0x08
+#define VIBRO_WAV_SEQ_REG6      0x09
+#define VIBRO_WAV_SEQ_REG7      0x0A
+#define VIBRO_WAV_SEQ_REG8      0x0B
 enum {
     WAIT_DEFAULT                = 0b00000000,
     WAIT_ENABLE                 = 0b10000000, // sets waveform sequencer to idle and delay is added between vibrations
@@ -37,7 +44,8 @@ enum {
 
 #define VIBRO_GO_REG            0x0C        //[0] sets vibro to go and gives waveforms to sequencer
 enum{
-    GO_DEFAULT                  = 0b00000000
+    VIBRO_GO                    = 0b00000001,
+    VIBRO_STOP                  = 0b00000000
 };
 
 //Offset registers
@@ -70,10 +78,11 @@ enum{
 enum{
     A_CAL_BEMF                  = 0b01101101
 };
-#define VIBRO_FEEDBACK_REG      0x1A        // all set before auto cal [7] 1:LRA mode [6:4] default 3 [3:2] default 1 [1:0] default 2
+#define VIBRO_FEEDBACK_REG      0x1A        
 enum{
     ERM_MODE                    = 0b00000000,
     LRA_MODE                    = 0b10000000,
+    
     FB_BRAKE_FACTOR_1x          = 0b00000000,
     FB_BRAKE_FACTOR_2x          = 0b00010000,
     FB_BRAKE_FACTOR_3x          = 0b00100000,
@@ -82,16 +91,18 @@ enum{
     FB_BRAKE_FACTOR_8x          = 0b01010000,
     FB_BRAKE_FACTOR_16x         = 0b01100000,
     FB_BRAKE_DIS                = 0b01110000,
+    
     LOOP_GAIN_LOW               = 0b00000000,
     LOOP_GAIN_MED               = 0b00000100, //default
     LOOP_GAIN_HIGH              = 0b00001000,
     LOOP_GAIN_VERY_HIGH         = 0b00010100,
+    
     BEMF_GAIN_LRA_5x            = 0b00000000,
     BEMF_GAIN_LRA_10x           = 0b00000001,
     BEMF_GAIN_LRA_20x           = 0b00000010, //default
     BEMF_GAIN_LRA_30x           = 0b00000000,
 }
-#define VIBRO_CONTROL1_REG      0x1B        // [5] 0:for PWM
+#define VIBRO_CONTROL1_REG      0x1B        
 enum{
     STARTUP_BOOST               = 0b10000000,
     AC_COUPLE_DIS               = 0b00000000, // for dc coupling or digital input modes
@@ -104,11 +115,14 @@ enum{
     INPUT_MODE_UNIDIR           = 0b00000000,
     INPUT_MODE_BIDIR            = 0b10000000,
     BRAKE_STABILISER            = 0b01000000,
+    
     SAMPLE_TIME_150um           = 0b00000000,
     SAMPLE_TIME_200um           = 0b00010000,
     SAMPLE_TIME_250um           = 0b00100000,
     SAMPLE_TIME_300um           = 0b00110000, //DEFAULT
+    
     BLANKING_TIME               = 0b00000100,
+    
     IDISS_TIME                  = 0b00000001
 };
 
@@ -118,15 +132,21 @@ enum{
     NG_THRESH_2pcnt             = 0b01000000,
     NG_THRESH_4pcnt             = 0b10000000, //DEFAULT
     NG_THRESH_8pcnt             = 0b11000000,
+    
     ERM_OPEN_LOOP               = 0b00100000, //DEFAULT N/A
+    
     SUPPLY_COMP_ENB             = 0b00000000,
     SUPPLY_COMP_DIS             = 0b00010000,
+    
     DATA_FORMAT_RTP_SNG         = 0b00000000, 
     DATA_FORMAT_RTP_USGN        = 0b00001000,
+    
     LRA_DRIVE_MODE_1            = 0b00000000,
     LRA_DRIVE_MODE_2            = 0b00000010,
+    
     INPUT_MODE_PWM              = 0b00000000,
     INPUT_MODE_ANALOG           = 0b00000010,
+    
     LRA_OPEN_LOOP               = 0b00000000,
     LRA_CLOSED_LOOP             = 0b00000001             
 };
