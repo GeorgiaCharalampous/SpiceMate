ed7713c321bf15ad234bd965a38908ecd3bc643d
GeorgiaCharalampous <101838741+GeorgiaCharalampous@users.noreply.github.com>
2024-04-09 19:06:27 +0100

    Test file addition

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -10,12 +10,13 @@ int main(int argc, char *argv[]){
     uint8_t hapticAmp = 128;
 
     motor_Instance.initVibro(Settings_Struct);
+    printf("Driver initialised \n");
     motor_Instance.playHaptic_realTime(hapticAmp);
     printf("Motor started \n");
     getchar();
-        motor_Instance.stopHaptic();
+    motor_Instance.stopHaptic();
     getchar();
-        motor_Instance.stop();
+    motor_Instance.stop();
     return 0;    
 
 
