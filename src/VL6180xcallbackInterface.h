2aa96ce0736531271ca7d2e65aa1e0397e6fee90
GeorgiaCharalampous <101838741+GeorgiaCharalampous@users.noreply.github.com>
2024-04-10 16:11:47 +0100

    Cleaning new branch

--- src/VL6180xcallbackInterface.h
+++
@@ -1,9 +0,0 @@
-class VL6180xcallback {
-/** 
- * Abstract callback interface.
- * Called after a sample has arrived.
- * \param sample contains sensor readings 
- **/
-	public:
-        virtual void hasSample(uint8_t sample) = 0;
-};
