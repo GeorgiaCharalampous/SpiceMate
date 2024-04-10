a7a24fdb61af593e02cd168e2a1bfe296fd0109f
GeorgiaCharalampous <101838741+GeorgiaCharalampous@users.noreply.github.com>
2024-04-10 21:46:14 +0100

    More attempts to trouble shoot

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -5,7 +5,11 @@ int main(int argc, char *argv[]){
     GROVE_EMG magnetInstance;
     GROVE_settings Settings_Struct;
 
+    printf("Hi");
+
     magnetInstance.initMagnet(Settings_Struct);
+
+    sleep(1);
 
     magnetInstance.startMagnet();
 
