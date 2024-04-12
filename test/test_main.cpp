0b65e3fd4d2a76e445d555ad0fea514a519dff06
ViktoriyaKaleva <viktoria.kaleva17@gmail.com>
2024-04-13 00:29:25 +0100

    Class works perfectly!

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -11,19 +11,13 @@ int main(int argc, char *argv[]){
 
     motor_Instance.initVibro(Settings_Struct);
     printf("Driver initialised \n");
-    // for (int i = 0; i < 100; i++)
-    // {
-    //     printf("... \n");
-    // };
 
-    //sleep(5);
     motor_Instance.playHaptic_realTime(hapticAmp);
     printf("Motor started \n");
     
     getchar();
     motor_Instance.stopHaptic();
     printf("Haptic stopped\n");
-    sleep(1);
     motor_Instance.stop();
     return 0;    
 }
