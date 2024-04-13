a1bd172441cf171c0e7000dacc50ea9ec6eb8f37
ViktoriyaKaleva <viktoria.kaleva17@gmail.com>
2024-04-13 14:08:14 +0100

    Comments and data protection of the VL6180x callback

--- src/VL6180xcallbackChild.h
+++ src/VL6180xcallbackChild.h
@@ -9,11 +9,24 @@
 #endif
 
 class VL6180xcallbackChild : public VL6180xcallback {
+
+	/**
+	 * Callback 
+	**/
 	virtual void hasSample(uint8_t sample);
-	DataProcess* dp = NULL;
-	
-	public:
+
+	/**
+	 * Registers the pointer to the data processing object 
+	**/
 	void registerDP(DataProcess* object){
 		dp = object;
 	};
+
+	private:
+	/**
+	 * Pointer to data processing object.
+	 * For passing to the next unit (class)
+	 * in the process chain.
+	**/
+	DataProcess* dp = NULL;
 };
